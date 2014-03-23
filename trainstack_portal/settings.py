aa7f467ba731207377186601b671490691f5750a
Nikolay Sokolov <chemikadze@gmail.com>
2014-03-23 19:39:45 +0400

    Fixed CSRF

--- trainstack_portal/settings.py
+++ trainstack_portal/settings.py
@@ -99,7 +99,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'trainstack_portal.middleware.DisableCSRF',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
